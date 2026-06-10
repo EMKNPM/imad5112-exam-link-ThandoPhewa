673e0b433d0df87d8976a15b93a0d80ffd8460c6
ThandoPhewa <ST10513456@myemeris.edu.za>
2026-06-10 13:38:01 +0200

    classes

---
+++ app/src/main/java/com/example/campsitecommander/Items.kt
@@ -0,0 +1,10 @@
+package com.example.campsitecommander
+
+class Items(
+    val itemNames: String,
+    val categories: String,
+    val comments: String,
+
+
+)
+
--- app/src/main/java/com/example/campsitecommander/ViewScreen.kt
+++ app/src/main/java/com/example/campsitecommander/ViewScreen.kt
@@ -1,5 +1,6 @@
 package com.example.campsitecommander
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -15,8 +16,6 @@ class ViewScreen : AppCompatActivity() {
     private lateinit var btnBack: Button
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,36 +25,58 @@ class ViewScreen : AppCompatActivity() {
         tvDetails = findViewById(R.id.tvDetails)
         btnBack = findViewById(R.id.btnBack)
 
-        var display = ""
+        //Parallel Array
+        val itemNames = arrayOf(
+            "Tent", "Sleeping bag", "Torch", "Camping chair", "First Aid kit"
+        )
+        val categories = arrayOf(
+            "Shelter", "Sleeping ", "Lighting", "Furniture", "Safety"
+        )
+        val quantities = arrayOf(
+            1, 2, 3, 2, 1
+        )
+        val comments = arrayOf(
+            "4 person tent",
+            "Warm weather bags",
+            "Extra batteries packed",
+            "Foldable chairs",
+            "Emergency supplies"
 
-        for (i in MainScreen.itemNames.indices){
 
-            display +=
-
-                "Items: ${MainScreen.itemNames[i]}\n" +
-                "Category: ${MainScreen.Categories[i]}\n"  +
-                "Quantity: ${MainScreen.Quantity[i]}\n" +
-                "Comments: ${MainScreen.Comments[i]}\n\n"
+        )
 
 
+        var displayText = "Camping Gear List\n\n"
 
+        for (i in Items.indices) {
+            displayText +=
+
+                "Items: ${itemNames[i]}\n" +
+                        "Category: ${categories[i]}\n" +
+                        "Quantity: ${quantities[i]}\n" +
+                        "Comments: ${comments[i]}\n\n"
+
+            tvDetails.text = displayText
+
+            btnBack.setOnClickListener {
+                startActivity(Intent(this, MainScreen::class.java))
+                finish()
+            }
+
+
+
+
+
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
 
-        tvDetails.text = display
+        class Items {
 
-        btnBack.setOnClickListener {
-            finish()
-        }
-
-
-
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
